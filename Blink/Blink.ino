3cdd943ad9c8b21fb9cf7ba0f384eb0ffe9268b2
Noralik <158551209+Noralik@users.noreply.github.com>
2024-03-12 15:12:56 +0200

    blink 4 diod

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,14 +1,31 @@
-
-// the setup function runs once when you press reset or power the board
+int redPin = 13;
+int yellowPin = 12;
+int greenPin = 11;
+int bluePin = 10;
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(yellowPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(redPin, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  digitalWrite(redPin, HIGH); 
+  delay(1000);
+  digitalWrite(redPin, LOW); 
+  delay(1000);   
+  digitalWrite(yellowPin, HIGH);   
+  delay(1000);    
+  digitalWrite(yellowPin, LOW);   
+  delay(1000);    
+  digitalWrite(greenPin, HIGH);  
+  delay(1000);    
+  digitalWrite(greenPin, LOW);  
+  delay(1000);    
+  digitalWrite(bluePin, HIGH);   
+  delay(1000);    
+  digitalWrite(bluePin, LOW); 
+  delay(1000);    
 }
